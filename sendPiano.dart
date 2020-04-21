21b0104ec263786d7551d4c5607de56145452e68
doyle-flutter <56661529+doyle-flutter@users.noreply.github.com>
2020-04-21 12:46:17 +0900

    Update sendPiano.dart

--- sendPiano.dart
+++ sendPiano.dart
@@ -1,7 +1,4 @@
-// 코드 정리 예정
-
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_socket_io/flutter_socket_io.dart';
 import 'package:flutter_socket_io/socket_io_manager.dart';
@@ -18,12 +15,14 @@ class _MyAppState extends State<MyApp> {
   SocketIO socketIO;
   List messages;
 
+  double get keyWidth => 80 + (80 * _widthRatio);
+  double _widthRatio = 0.0;
+
   @override
   initState() {
     messages = List();
-
     socketIO = SocketIOManager().createSocketIO(
-      'http://:8808',
+      'http://192.168.0.2:8808',
       '/',
     )
       ..init()
@@ -39,25 +38,28 @@ class _MyAppState extends State<MyApp> {
     super.initState();
   }
 
-  double get keyWidth => 80 + (80 * _widthRatio);
-  double _widthRatio = 0.0;
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'The Pocket Piano',
       theme: ThemeData.dark(),
       home: Scaffold(
-        appBar: AppBar(title: Text("SEND"),),
-          body: ListView.builder(
-            itemCount: 7,
-            controller: ScrollController(initialScrollOffset: 1500.0),
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (BuildContext context, int index) {
-              final int i = index * 12;
-              return SafeArea(
-                child: Stack(children: <Widget>[
-                  Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+        appBar: AppBar(
+          title: Text("SEND"),
+        ),
+        body: ListView.builder(
+          itemCount: 7,
+          controller: ScrollController(
+            initialScrollOffset: 1500.0
+          ),
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (BuildContext context, int index) {
+            final int i = index * 12;
+            return SafeArea(
+              child: Stack(children: <Widget>[
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
                     _buildKey(24 + i, false),
                     _buildKey(26 + i, false),
                     _buildKey(28 + i, false),
@@ -65,91 +67,106 @@ class _MyAppState extends State<MyApp> {
                     _buildKey(31 + i, false),
                     _buildKey(33 + i, false),
                     _buildKey(35 + i, false),
-                  ]),
-                  Positioned(
-                      left: 0.0,
-                      right: 0.0,
-                      bottom: 100,
-                      top: 0.0,
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            Container(width: keyWidth * .5),
-                            _buildKey(25 + i, true),
-                            _buildKey(27 + i, true),
-                            Container(width: keyWidth),
-                            _buildKey(30 + i, true),
-                            _buildKey(32 + i, true),
-                            _buildKey(34 + i, true),
-                            Container(width: keyWidth * .5),
-                          ])),
-                ]),
-              );
-            },
-          )),
+                  ]
+                ),
+                Positioned(
+                  left: 0.0,
+                  right: 0.0,
+                  bottom: 100,
+                  top: 0.0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Container(width: keyWidth * .5),
+                      _buildKey(25 + i, true),
+                      _buildKey(27 + i, true),
+                      Container(width: keyWidth),
+                      _buildKey(30 + i, true),
+                      _buildKey(32 + i, true),
+                      _buildKey(34 + i, true),
+                      Container(width: keyWidth * .5),
+                    ]
+                  )
+                ),
+              ]
+            ),
+          );
+          }
+        )
+      ),
     );
   }
 
   Widget _buildKey(int midi, bool accidental) {
-
     final pitchName = Pitch.fromMidiNumber(midi).toString();
     final pianoKey = Stack(
       children: <Widget>[
         Semantics(
-            button: true,
-            hint: pitchName,
-            child: Material(
-                borderRadius: borderRadius,
-                color: accidental ? Colors.black : Colors.white,
-                child: InkWell(
-                  borderRadius: borderRadius,
-                  highlightColor: Colors.grey,
-                  onTap: ()async{
-                    await Future.microtask((){
-                      socketIO.sendMessage(
-                          'send_message', json.encode(
-                          {
-                            "message": midi,
-                            "name" :"James"
-                          }
-                      ));
-                      return;
-                    });
-                  },
-                ))),
-        Positioned(
-            left: 0.0,
-            right: 0.0,
-            bottom: 20.0,
-            child: Text(pitchName,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: !accidental ? Colors.black : Colors.white
-                )
+          button: true,
+          hint: pitchName,
+          child: Material(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(10.0),
+              bottomRight: Radius.circular(10.0)
+            ),
+            color: accidental ? Colors.black : Colors.white,
+            child: InkWell(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10.0),
+                bottomRight: Radius.circular(10.0)
+              ),
+              highlightColor: Colors.grey,
+              onTap: ()async{
+                await Future.microtask((){
+                  socketIO.sendMessage(
+                    'send_message', json.encode(
+                    {
+                      "message": midi,
+                      "name" :"James"
+                    }
+                  ));
+                  return;
+                });
+              },
             )
+          )
+        ),
+        Positioned(
+          left: 0.0,
+          right: 0.0,
+          bottom: 20.0,
+          child: Text(
+            pitchName,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: !accidental ? Colors.black : Colors.white
+            )
+          )
         ),
       ],
     );
+
     if (accidental) {
       return Container(
-          width: keyWidth,
-          margin: EdgeInsets.symmetric(horizontal: 2.0),
-          padding: EdgeInsets.symmetric(horizontal: keyWidth * .1),
-          child: Material(
-              elevation: 6.0,
-              borderRadius: borderRadius,
-              shadowColor: Color(0x802196F3),
-              child: pianoKey
-          )
+        width: keyWidth,
+        margin: EdgeInsets.symmetric(horizontal: 2.0),
+        padding: EdgeInsets.symmetric(horizontal: keyWidth * .1),
+        child: Material(
+          elevation: 6.0,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(10.0),
+            bottomRight: Radius.circular(10.0)
+          ),
+          shadowColor: Color(0x802196F3),
+          child: pianoKey
+        )
       );
     }
     return Container(
-        width: keyWidth,
-        child: pianoKey,
-        margin: EdgeInsets.symmetric(horizontal: 2.0));
+      width: keyWidth,
+      child: pianoKey,
+      margin: EdgeInsets.symmetric(horizontal: 2.0)
+    );
   }
 }
-
-const BorderRadiusGeometry borderRadius = BorderRadius.only(
-    bottomLeft: Radius.circular(10.0), bottomRight: Radius.circular(10.0));
