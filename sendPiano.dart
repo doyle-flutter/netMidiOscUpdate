b90a32eb619a4ee8fa52c4f5912397b73aeb9934
doyle-flutter <56661529+doyle-flutter@users.noreply.github.com>
2020-04-21 12:46:48 +0900

    Update sendPiano.dart

--- sendPiano.dart
+++ sendPiano.dart
@@ -22,7 +22,7 @@ class _MyAppState extends State<MyApp> {
   initState() {
     messages = List();
     socketIO = SocketIOManager().createSocketIO(
-      'http://192.168.0.2:8808',
+      'http://127.0.0.1:8808',
       '/',
     )
       ..init()
